b47373e4d90edee9bb26477c066c53bbe2fa5a50
jonathanmir <jonathanmir8@gmail.com>
2022-08-08 15:52:28 -0300

    entrega

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Jonathan";
+let sobreNome = "de Souza Miranda";
+let anoNascimento = 1999;
+let anoAtual = 2022;
+let idade = anoAtual - anoNascimento;
+let cidade = "Ipanema";
+console.log(
+  `Eu sou o ${nome} ${sobreNome}, tenho ${
+    anoAtual - anoNascimento
+  } anos e moro em ${cidade}.`
+);
